85a512e8f686fdb793fa925ecb7bfd152543cdce
youpy <youpy@buycheapviagraonlinenow.com>
2022-01-02 00:30:03 +0900

    add tests

--- src/event.test.ts
+++ src/event.test.ts
@@ -1,18 +1,54 @@
-import { Gender } from "./birthday";
-import { Event } from "./event";
+import { Gender, IBirthDay } from "./birthday";
+import { Event, IEvent, IEventApplication } from "./event";
 
 describe("event", () => {
   test("applies to a birthday", () => {
-    const event = new Event(0, 0, 31, null, null, false, "お宮参り・初宮参り");
-    const birthday = {
+    const earlyBirthday = {
       day: "2016-02-10",
       name: "foo",
       gender: "m" as Gender,
     };
-    const res = event.apply(birthday);
+    const lateBirthday = {
+      day: "2016-08-18",
+      name: "bar",
+      gender: "f" as Gender,
+    };
+    const tests: [IBirthDay, IEvent, IEventApplication | null][] = [
+      [
+        earlyBirthday,
+        new Event(0, 0, 31, null, null, false, "お宮参り・初宮参り"),
+        {
+          date: new Date(Date.parse("2016-03-12T00:00:00.000Z")),
+          summary: "foo お宮参り・初宮参り",
+        },
+      ],
+      [
+        earlyBirthday,
+        new Event(3, 0, 0, 10, 14, true, "七五三 3歳", "f"),
+        null,
+      ],
+      [
+        earlyBirthday,
+        new Event(6, 0, 0, 3, 0, true, "小学校入学"),
+        {
+          date: new Date(Date.parse("2022-04-01T00:00:00.000Z")),
+          summary: "foo 小学校入学",
+        },
+      ],
+      [
+        lateBirthday,
+        new Event(6, 0, 0, 3, 0, true, "小学校入学"),
+        {
+          date: new Date(Date.parse("2023-04-01T00:00:00.000Z")),
+          summary: "bar 小学校入学",
+        },
+      ],
+    ];
 
-    expect(res).not.toBeNull();
-    expect(res?.date.toISOString()).toBe("2016-03-12T00:00:00.000Z");
-    expect(res?.summary).toBe("foo お宮参り・初宮参り");
+    tests.forEach(([bd, ev, expected]) => {
+      const actual = ev.apply(bd);
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
--- src/schedule.test.ts
+++ src/schedule.test.ts
@@ -51,25 +51,6 @@ describe("schedule", () => {
     expect(icalEvents[1].summary).toBe("foo bar 小学校入学");
   });
 
-  test("creates a schedule with early birthday", () => {
-    const bd = { name: "foo bar", day: "2016-04-30", gender: "m" as Gender };
-    const events: Event[] = [new Event(6, 0, 0, 3, 0, true, "小学校入学")];
-    const schedule = new Schedule(bd);
-
-    events.forEach((ev) => {
-      schedule.add(ev);
-    });
-
-    const icalEvents = extractEvents(schedule.toString());
-
-    expect(icalEvents.length).toBe(1);
-    expect(icalEvents[0].start?.hasOwnProperty("dateOnly")).toBe(true);
-    expect(icalEvents[0].start?.getDate()).toBe(1);
-    expect(icalEvents[0].start?.getFullYear()).toBe(2023);
-    expect(icalEvents[0].start?.getMonth()).toBe(3);
-    expect(icalEvents[0].summary).toBe("foo bar 小学校入学");
-  });
-
   test("creates a schedule with a event for a specific gender", () => {
     const bd = { name: "foo bar", day: "2016-04-30", gender: "f" as Gender };
     const events: Event[] = [
